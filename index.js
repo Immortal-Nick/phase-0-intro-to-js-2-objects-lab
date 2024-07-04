dd0604694035381ab232a38dd568b0d65edce7a8
Nixon <kiplangatnixon28@gmail.com>
2024-07-04 12:35:54 +0300

    modifies objects

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name: "fay",
+    streetAdress: "307 pipeline",
+}
+
+//it does not modify the original employee, but rather returns a clone with the new data
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+
+  // updates `employee` with the given `key` and `value` (it is destructive) and returns the entire updated employee
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    
+    // Return the updated employee object
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    // Create a shallow copy of the employee object
+    let newEmployee = { ...employee };
+    
+    // Delete the specified key from the cloned object
+    delete newEmployee[key];
+    
+    // Return the new cloned object
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the specified key from the original employee object
+    delete employee[key];
+    
+    // Return the modified employee object
+    return employee;
+  }
